ebb5d8c2d921571824e84c1207e9233ef1e2db60
pablo.iglesias <pablo.iglesias@106-13.info2>
2024-10-08 14:00:21 +0200

    Ejer5 Corregido

--- src/boletin1/Ejer5.java
+++ src/boletin1/Ejer5.java
@@ -26,22 +26,18 @@ public class Ejer5 {
 		// Creamos condicional
 		if (num1 >= num2 && num2 >= num3) {
 			System.out.println(num1 + "," + num2 + "," + num3);
-		} else if (num2>=num1 && num1>=num3) {
+		} else if (num2 >= num1 && num1 >= num3) {
+			System.out.println(num2 + "," + num1 + "," + num3);
+		} else if (num1 >= num3 && num3 >= num2) {
 			System.out.println(num1 + "," + num3 + "," + num2);
-		} else if (num2>=num1 && num1>=num3) {
-			System.out.println(num2 + "," + num1 + "," + num3);
-		} else if (num2>=num1 && num1>=num3) {
-			System.out.println(num2 + "," + num1 + "," + num3);
-		} else if (num2>=num1 && num1>=num3) {
-			System.out.println(num2 + "," + num1 + "," + num3);
-		} else if (num2>=num1 && num1>=num3) {
-			System.out.println(num2 + "," + num1 + "," + num3);
-		} else if (num2>=num1 && num1>=num3) {
-			System.out.println(num2 + "," + num1 + "," + num3);
-		} else if (num2>=num1 && num1>=num3) {
-			System.out.println(num2 + "," + num1 + "," + num3);
-			
-			
+		} else if (num2 >= num3 && num3 >= num1) {
+			System.out.println(num2 + "," + num3 + "," + num1);
+		} else if (num3 >= num1 && num1 >= num2) {
+			System.out.println(num3 + "," + num1 + "," + num2);
+		} else if (num3 >= num2 && num2 >= num1) {
+			System.out.println(num3 + "," + num2 + "," + num1);
 		}
+		// Cerramos el Scanner.
+		sc.close();
 	}
 }
