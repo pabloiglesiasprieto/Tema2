b8a6497bc13107dcce84d899e311f00cb2033086
Pablo <Pablo@192.168.1.160>
2024-10-15 20:49:05 +0200

    a

---
+++ src/bucles/Ejer6.java
@@ -0,0 +1,14 @@
+package bucles;
+
+public class Ejer6 {
+	public static void main(String[] args) {
+		/*
+		 * Un centro de investigación de la flora urbana necesita una aplicación que
+		 * muestre cuál es el árbol más alto. Para ello se introducirá por teclado la
+		 * altura (en centímetros) de cada árbol (terminando la introducción de datos
+		 * cuando se utilice -1 como altura). La aplicación debe devolver la altura del
+		 * árbol más alto.
+		 */
+		// 
+	}
+}
